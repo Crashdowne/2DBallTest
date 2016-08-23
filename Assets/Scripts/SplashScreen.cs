ef8e9bd9a2da09e1a117b72b2a241d6fffa7f415
Bryan <bkesteloo@outlook.com>
2016-08-22 19:11:21 -0700

    More UI Work

---
+++ Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+	// Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(WaitToStart());      
+    }
+
+    IEnumerator WaitToStart()
+    {
+        Debug.Log("waiting");
+        yield return new WaitForSeconds(1.0f);
+        Debug.Log("wait end");
+        SceneManager.LoadScene("Menu");
+    }
+}
