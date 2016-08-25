fc299ff57b9f57bb474abb46d1ff6404a549fd9d
Bryan <bkesteloo@outlook.com>
2016-08-24 17:57:26 -0700

    Fixed a conflict with splashscreen

--- Assets/Scripts/Splash.cs
+++ Assets/Scripts/Splash.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class SplashScreen : MonoBehaviour {
+public class Splash : MonoBehaviour {
 
 	// Use this for initialization
     void Start ()
